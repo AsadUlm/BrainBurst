60c2b3b768e2e26959bc1a3d39403cb5a8b69d9f
AsadUlm <91129806+AsadUlm@users.noreply.github.com>
2023-07-31 15:06:40 +0900

    Login resuld add nullguard

--- BrainBurst.Domain/Model/LoginResult.cs
+++ BrainBurst.Domain/Model/LoginResult.cs
@@ -1,7 +1,10 @@
-﻿namespace BrainBurst.Domain.Model;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BrainBurst.Domain.Model;
 public class LoginResult
 {
     public bool Successful { get; set; }
-    public string Error { get; set; }
+    [AllowNull]
+    public string? Error { get; set; }
     public string Token { get; set; }
 }
